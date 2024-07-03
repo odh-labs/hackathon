3663003de3ef60872fe16562e3c58a2f694bff59
James Blair <mail@jamesblair.net>
2024-07-03 12:04:42 +1200

    Fix 'Environment login' header link.

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
     { href: '/hackathon', title: 'Exercises' },
     { href: 'https://docs.openshift.com/container-platform/4.15/welcome/index.html', title: 'Documentation' },
-    { href: 'https://demo.redhat.com/workshop/s72ya3', title: 'Environment login' }
+    { href: 'https://demo.redhat.com/workshop/z567br', title: 'Environment login' }
 ]
 
 export default headerNavLinks
